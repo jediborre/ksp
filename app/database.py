5acc664a31a229040500eeba6f84a1d602c2fcf0
Fernando Borrego <fernando.borrego@gmail.com>
2023-01-26 00:18:52 -0600

    creacion sesion DB

--- app/database.py
+++ app/database.py
@@ -1,17 +1,24 @@
 from .config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
-# from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
 
 MYSQL_DATABASE_URL = f"mysql://"\
                     f"{settings.MYSQL_USERNAME}:{settings.MYSQL_PASSWORD}@"\
                     f"{settings.MYSQL_HOSTNAME}:{settings.MYSQL_PORT}/"\
                     f"{settings.MYSQL_DB}"
 
+Base = declarative_base()
 engine = create_engine(MYSQL_DATABASE_URL)
 
 Base.metadata.create_all(bind=engine)
-session = Session(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
