f6b26e1780ae2ca590997252d4132f8585d144f2
Fernando Borrego <fernando.borrego@gmail.com>
2023-01-30 00:43:19 -0600

    deploy

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+cd front
+npm run build
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:jediborre/ksp.git main:gh-pages
+
+cd -
