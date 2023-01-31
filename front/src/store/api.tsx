0649997e7634b19ff5e3ee65ab01fe031beadb69
Fernando Borrego <fernando.borrego@gmail.com>
2023-01-31 12:49:35 -0600

    fix cors

--- front/src/store/api.tsx
+++ front/src/store/api.tsx
@@ -26,7 +26,7 @@ export interface beneficiario {
 }
 type Beneficiarios = beneficiario[];
 
-const baseUrl = 'http://localhost:8000/';
+const baseUrl = 'http://localhost:8080/';
 
 export const apiSlice = createApi({
   reducerPath: 'Empleados',
